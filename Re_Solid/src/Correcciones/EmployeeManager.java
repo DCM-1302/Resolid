2eea813031cb826020436ffef27289ba5dd8ff77
DCM-1302 <124814673+DCM-1302@users.noreply.github.com>
2024-08-02 21:40:08 -0500

    Update EmployeeManager.java

--- Re_Solid/src/Correcciones/EmployeeManager.java
+++ Re_Solid/src/Correcciones/EmployeeManager.java
@@ -9,13 +9,4 @@ public class EmployeeManager implements EmployeeOperations {
     public void removeEmployee(String name) {
         // Eliminar empleado
     }
-
-    public double calculateSalary(String name) {
-        // Calcular salario
-        return 0.0;
-    }
-
-    public void saveToFile(String name) {
-        // Guardar en archivo
-    }
 }
