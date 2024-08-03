8a818813508b36b402a53676861c93672ca8d125
DCM-1302 <124814673+DCM-1302@users.noreply.github.com>
2024-08-02 21:21:33 -0500

    Update PartTimeEmployee.java
    Corrección part timer

--- Re_Solid/src/Correcciones/PartTimeEmployee.java
+++ Re_Solid/src/Correcciones/PartTimeEmployee.java
@@ -3,6 +3,6 @@ package Correcciones;
 public class PartTimeEmployee extends Employee {
     @Override
     public String getName() {
-        throw new UnsupportedOperationException();
+        return "Patricio";
     }
 }
