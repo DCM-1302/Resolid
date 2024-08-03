b134305565daede40711e89310f34a0dccb7ccd6
DCM-1302 <124814673+DCM-1302@users.noreply.github.com>
2024-08-02 21:41:07 -0500

    Update EmployeeOperations.java

--- Re_Solid/src/Correcciones/EmployeeOperations.java
+++ Re_Solid/src/Correcciones/EmployeeOperations.java
@@ -4,8 +4,4 @@ public interface EmployeeOperations {
     void addEmployee(String name);
 
     void removeEmployee(String name);
-
-    double calculateSalary(String name);
-
-    void saveToFile(String name);
 }
