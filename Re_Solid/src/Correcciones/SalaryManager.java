d71c3415641ddb4ca6fc4c9c9c33f5cb1c0bdab8
DCM-1302 <124814673+DCM-1302@users.noreply.github.com>
2024-08-02 21:48:36 -0500

    Add files via upload

---
+++ Re_Solid/src/Correcciones/SalaryManager.java
@@ -0,0 +1,9 @@
+package Correcciones;
+
+public class SalaryManager implements SalaryOperations{
+
+    public double calculateSalary(String name) {
+        // Calcular salario
+        return 0.0;
+    }
+}
